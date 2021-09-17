c835877dc1d99e13824ef0aaab2e6033ae7f6690
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-09-17 09:16:33 -0700

    Update

--- podman-machine-setup.sh
+++ podman-machine-setup.sh
@@ -3,7 +3,7 @@
 
 podman machine stop podman-machine-default
 sleep 5
-podman machine rm --force podman-machine-default <y
+podman machine rm --force podman-machine-default 
 podman machine init --cpus 4 --disk-size 128 --memory 4096 podman-machine-default
 podman machine start podman-machine-default
 sleep 5
